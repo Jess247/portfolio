da28b365c64d9a7354437d6295ae1cf941996b58
Jessica <jesss8692@hotmail.com>
2024-11-01 13:30:23 +0100

    Add SocialNav component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
+import SocialNav from "./SocialNav"
+
 function Header() {
     return(
-        <header className="prose text-white">
+        <header className="text-white">
             <h1 className="">Jessica Singletary</h1>
             <h2>Frontend Developer</h2>
             <p>I build responsive, modern and accessible website applications.</p>
+            <SocialNav/>
         </header>
     )
 }
---
+++ src/components/SocialNav.jsx
@@ -0,0 +1,15 @@
+import { FaGithubSquare } from "react-icons/fa"
+import { FaLinkedin } from "react-icons/fa"
+import { MdEmail } from "react-icons/md";
+
+function SocialNav() {
+    return (
+        <nav>
+            <FaGithubSquare />
+            <FaLinkedin />
+            <MdEmail />
+        </nav>
+    )
+}
+
+export default SocialNav
