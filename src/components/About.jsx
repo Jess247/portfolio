fb939fe8c90c55a057748cb109552d7affb7b059
Jessica <jesss8692@hotmail.com>
2024-11-01 18:09:14 +0100

    Style about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,14 @@
 function About() {
     return(
         <article>
-            <h3>About</h3>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
+            <h3 className="text-xl font-semibold uppercase tracking-wider mb-4">About</h3>
+            <p className="text-slate-400 font-thin leading-6">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
                 tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
                 At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
                  no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, 
                 consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.
             </p>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
+            <p className="text-slate-400 font-thin leading-6">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
                 tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
                 At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
                  no sea takimata sanctus est Lorem ipsum dolor sit amet.
