4e8dabd1b813e5f86551a67468ba1beb20462806
Jessica <jesss8692@hotmail.com>
2024-11-01 13:24:52 +0100

    Add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return(
+        <header className="prose text-white">
+            <h1 className="">Jessica Singletary</h1>
+            <h2>Frontend Developer</h2>
+            <p>I build responsive, modern and accessible website applications.</p>
+        </header>
+    )
+}
+
+export default Header
