bb42dfb62612b5a10e4f0bf3a606820c4750dd85
Jessica <jesss8692@hotmail.com>
2024-11-01 21:31:58 +0100

    Update media queries

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,7 +1,11 @@
 function Badge({badgeText}){
     return(
-        <div >
-            {badgeText}
+        <div className="flex gap-2" >
+            {badgeText.map(badge => {
+                return(
+                    <span className="bg-violet-400 py-1 px-2 rounded-full text-violet-950 text-sm font-light my-2">{badge}</span>
+                    )}
+            )}
         </div>
     )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,18 +4,20 @@ import Button from './Button'
 function Card({imgSrc, altText}) {
     return(
         <div className='relative my-24'>
-            <div className='absolute inset-0 bg-violet-900 rounded-lg blur'></div>
-            <div className='relative z-2 bg-neutral-950 p-5 rounded-lg flex flex-col gap-4'>
-                <h4 className='text-lg font-semibold'>React Quiz</h4>
-                <p className='font-thin text-slate-400'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-                    nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-                    sed diam voluptua.
-                </p>
-                <div>
-                    <Button btnTxt="View live"/>
-                    <Button btnTxt="View code"/>
+            <div className='absolute inset-0 bg-gray-900 md:bg-violet-900 rounded-lg blur'></div>
+            <div className='relative z-2 bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] p-5 rounded-lg flex flex-col gap-4 md:flex-row md:items-center'>
+                <div className='flex flex-col gap-3'>
+                    <h4 className='text-lg font-semibold'>React Quiz</h4>
+                    <p className='font-thin text-slate-400 max-w-[600px]'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+                        sed diam voluptua.
+                    </p>
+                    <div className='my-2'>
+                        <Button btnTxt="View live"/>
+                        <Button btnTxt="View code"/>
+                    </div>
+                    <Badge badgeText={["HTML5", "SASS/CSS","JavaScript", "React"]} />
                 </div>
-                <Badge badgeText={["HTML5", "SASS/CSS","JavaScript", "React"]} />
                 <img 
                     className="w-1/2 rounded-lg shadow-sm shadow-gray-800"
                     src={imgSrc} 
