79f127e55996e0066d773dba4b7e949cb37dfea2
Jessica <jesss8692@hotmail.com>
2024-11-01 14:00:17 +0100

    Add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header"
 import About from "./components/About"
 import Card from "./components/Card"
 import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <About/>
       <Card/>
       <Contact/>
+      <Footer/>
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import SocialNav from "./SocialNav"
+
+function Footer() {
+    return(
+        <footer>
+            <SocialNav/>
+            <span>&copy; Jessica Singletary</span>
+        </footer>
+    )
+}
+
+export default Footer
