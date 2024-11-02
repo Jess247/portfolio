fc2218d3a9aafa7479843bf94b40318cd62e1d9a
Jessica <jesss8692@hotmail.com>
2024-11-02 19:51:11 +0100

    Add project imges

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import About from "./components/About"
 import Card from "./components/Card"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
+import gif from "./assets/imgs/gif.png"
+import quiz from "./assets/imgs/quiz.png"
+import movie from "./assets/imgs/movie.png"
 
 function App() {
 
@@ -10,7 +13,7 @@ function App() {
       {
         id: 0,
         name: "React Quiz",
-        img: "https://plus.unsplash.com/premium_photo-1663050633633-2856e875dcc7?w=1200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D",
+        img: `${quiz}`,
         altText: "Project thumbnail.",
         description: "lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
         code: "https://github.com",
@@ -19,8 +22,8 @@ function App() {
       },
       {
         id: 1,
-        name: "Chrome extension",
-        img: "https://plus.unsplash.com/premium_photo-1678565879444-f87c8bd9f241?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fHww",
+        name: "Movie List",
+        img: `${movie}`,
         altText: "Project thumbnail.",
         description: "lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
         code: "https://github.com",
@@ -30,7 +33,7 @@ function App() {
       {
         id: 2,
         name: "Gif Generator",
-        img: "https://images.unsplash.com/photo-1602265568624-29e8dc535bd6?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fHww",
+        img: `${gif}`,
         altText: "Project thumbnail.",
         description: "lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
         code: "https://github.com",
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import SocialNav from "./SocialNav"
 function Header() {
     return(
         <header className="mt-2 mb-2 mr-4">
-            <h1 className="text-4xl font-bold mb-8">Jessica <span className=" text-violet-400 [text-shadow:_1px_1px_5px_rgb(179_74_224_/_40%)]">Singletary</span></h1>
+            <h1 className="text-4xl font-bold mb-8 ">Jessica <span className=" font-normal text-violet-400 [text-shadow:_1px_1px_5px_rgb(179_74_224_/_40%)]">Singletary</span></h1>
             <h2 className="text-xl leading-[3rem]">Frontend Developer</h2>
             <p className="text-slate-400 font-thin mb-16">I build responsive, modern and accessible website applications.</p>
             <SocialNav/>
